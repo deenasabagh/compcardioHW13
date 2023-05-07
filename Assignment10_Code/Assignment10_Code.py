274ab05dfa138de5a7885cb80821c3e433dfb9fc
Deena <deena.sabagh@yahoo.com>
2023-05-07 15:55:35 -0400

    yes

--- Assignment10_Code/Assignment10_Code.py
+++ Assignment10_Code/Assignment10_Code.py
@@ -28,9 +28,35 @@ for i in range(run_to+1):
         plt.figure(10)
         nx.draw_networkx(Q, node_color=f, pos=pos_d, with_labels=False, vmin=0,vmax=.25)
         plt.title('time = '+str(i))
-        plt.savefig('path/to/save/at'+'time'+str(j)+'.png') # change (path/to/save/at/) to path for a new folder 
+        plt.savefig('outputs/'+'time'+str(j)+'.png')
         
         j += 1
 
     f = f - dt*np.matmul(lap,f)
+    
+import numpy as np
+
+# Extract temperature values at tstep = 0, 50, and 100
+tstep0_temp = f[0, :]
+tstep50_temp = f[50, :]
+tstep100_temp = f[100, :]
+
+# Calculate mean and standard deviation using NumPy functions
+tstep0_mean = np.mean(tstep0_temp)
+tstep50_mean = np.mean(tstep50_temp)
+tstep100_mean = np.mean(tstep100_temp)
+
+tstep0_std = np.std(tstep0_temp)
+tstep50_std = np.std(tstep50_temp)
+tstep100_std = np.std(tstep100_temp)
+
+# Print the results
+print("Mean temperature at tstep = 0: {:.2f}".format(tstep0_mean))
+print("Standard deviation of temperature at tstep = 0: {:.2f}".format(tstep0_std))
+
+print("Mean temperature at tstep = 50: {:.2f}".format(tstep50_mean))
+print("Standard deviation of temperature at tstep = 50: {:.2f}".format(tstep50_std))
+
+print("Mean temperature at tstep = 100: {:.2f}".format(tstep100_mean))
+print("Standard deviation of temperature at tstep = 100: {:.2f}".format(tstep100_std))
  
